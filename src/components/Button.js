cef7848b9c133da872627b74dce46f9d051b7b1a
Max Lettenberger <maxlettenberger@gmail.com>
2022-04-08 10:36:30 -0400

    add configs

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const test = 'summm';
+
+const Button = () => {
+  return <button>My Button Changed Again !!!! </button>;
+};
+
+export default Button;
---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './components/Button';
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Hello!!!!! {this.props.name}</div>
+        <Button />
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Max" />,
+  document.getElementById('hello-example')
+);
+
+const max = { name: 'Max' };
